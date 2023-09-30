64cbe07089db9ae757cb6ce8e94ef88e742f56e5
jihun <jihun315016@gmail.com>
2023-09-30 21:17:19 +0900

    acm_1100

---
+++ acm/acm_1100.cs
@@ -0,0 +1,41 @@
+// 백준 1100 하얀 칸
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int cnt = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                line = Console.ReadLine();
+                cnt = cnt + GetCount(line, i % 2);
+            }
+
+            Console.WriteLine(cnt);
+        }
+
+        /// <summary>
+        /// 하얀 칸 위에 말의 개수 출력
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="start">하얀 칸이 있는 시작 지점</param>
+        /// <returns></returns>
+        static int GetCount(string line, int start)
+        {
+            int cnt = 0;
+            // i는 하얀칸
+            for (int i = start; i < 8; i = i + 2)
+            {
+                if (line[i].Equals('F'))
+                {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+}

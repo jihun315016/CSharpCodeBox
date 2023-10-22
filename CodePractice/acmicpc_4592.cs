68ce8494e097ce62d31d4b8e462f5cb624bb513a
jihun <jihun315016@gmail.com>
2023-10-22 14:39:24 +0900

    acmicpc_4592

---
+++ CodePractice/acmicpc_4592.cs
@@ -0,0 +1,36 @@
+// 백준 4592 중복을 없애자
+
+namespace ConsoleApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        int n;
+         
+        while (true)
+        {
+            list = (
+                from i
+                in Console.ReadLine().Split()
+                select Convert.ToInt32(i)
+                ).ToList<int>();
+            
+            if (list[0] == 0) 
+                break;
+
+            n = -1;
+            for (int i = 1; i < list.Count; i++) 
+            {
+                if (list[i] != n)
+                {
+                    n = list[i];
+                    Console.Write($"{list[i]} ");
+                }
+            }
+
+            Console.WriteLine("$");
+        }
+    }
+}

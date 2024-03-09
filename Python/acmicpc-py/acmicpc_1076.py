090fd7478c83fb1fb14e8a906e4b047b16d6f6ac
jihun <jihun315016@gmail.com>
2024-03-09 22:14:14 +0900

    acmicpc-py 1076

---
+++ Python/acmicpc-py/acmicpc_1076.py
@@ -0,0 +1,24 @@
+# 1076 저항
+
+import sys
+
+input = sys.stdin.readline
+
+dic = {
+    "black" : 0,
+    "brown" : 1,
+    "red" : 2,
+    "orange" : 3,
+    "yellow" : 4,
+    "green" : 5,
+    "blue" : 6,
+    "violet" : 7,
+    "grey" : 8,
+    "white" : 9
+}
+
+a = input().rstrip()
+b = input().rstrip()
+c = input().rstrip()
+
+print((dic[a] * 10 + dic[b]) * 10 ** dic[c])

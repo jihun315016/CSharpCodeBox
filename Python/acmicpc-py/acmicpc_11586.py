0c20136b851131c30d21fd7c0413d7d1ab674c36
jihun <jihun315016@gmail.com>
2024-03-19 21:18:10 +0900

    acmicpc-py 11586

---
+++ Python/acmicpc-py/acmicpc_11586.py
@@ -0,0 +1,15 @@
+# 11586 지영 공주님의 마법 거울
+
+n = int(input())
+arr = [input() for _ in range(n)]
+k = int(input())
+
+if k == 1:
+    for a in arr:
+        print(a)
+elif k == 2:
+    for a in arr:
+        print(a[::-1])
+else:
+    for i in range(len(arr) - 1, -1, -1):
+        print(arr[i])
